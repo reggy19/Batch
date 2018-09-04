31432bba8f514801da61f79f27247ccf5f584283
reggy19 <arwatson19@gmail.com>
2018-09-04 18:18:35 +0530

    timedelay

---
+++ timedelay.bat
@@ -0,0 +1,13 @@
+@echo off
+D:
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+call reggyhome.bat
+call printwords.bat #Time_Delay
+
+set /p waitminutes=Enter minutes
+set /a waitminutes=60*%waitminutes%
+if "%waitminutes%" == "" (
+	echo no timeout specified
+) else (
+	timeout %waitminutes%
+)
